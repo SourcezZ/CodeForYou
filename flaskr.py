77510a630c1f58fd4d642419811b558574f162b9
heyuan <heyuan@csii.com.cn>
2018-08-21 16:28:36 +0800

    note init_db()

--- flaskr.py
+++ flaskr.py
@@ -130,11 +130,11 @@ def upload_file():
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-def init_db():
-        with closing(connect_db()) as db:
-            with app.open_resource('schema.sql') as f:
-                db.cursor().executescript(f.read().decode())
-            db.commit()
+# def init_db():
+#         with closing(connect_db()) as db:
+#             with app.open_resource('schema.sql') as f:
+#                 db.cursor().executescript(f.read().decode())
+#             db.commit()
 
 @app.before_request
 def before_request():
